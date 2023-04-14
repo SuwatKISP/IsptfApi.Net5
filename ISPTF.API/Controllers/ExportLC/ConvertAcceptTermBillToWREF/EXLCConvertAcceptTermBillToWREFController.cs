d2ff65274ee544868edfb988087df47e73bf65de
Natchapol <natchapol@findx.co.th>
2023-04-14 15:31:29 +0700

    Finishing LC 07 - EXLC Convert Accept Bill to WREF

--- ISPTF.API/Controllers/ExportLC/ConvertAcceptTermBillToWREF/EXLCConvertAcceptTermBillToWREFController.cs
+++ ISPTF.API/Controllers/ExportLC/ConvertAcceptTermBillToWREF/EXLCConvertAcceptTermBillToWREFController.cs
@@ -11,7 +11,8 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
- 
+using System.Text.Json;
+
 namespace ISPTF.API.Controllers.ExportLC
 {
     [ApiController]
@@ -25,31 +26,64 @@ namespace ISPTF.API.Controllers.ExportLC
         }
 
         [HttpGet("list")]
-        public async Task<IEnumerable<Q_EXLCAcceptTermDueListPageRsp>> GetAllList(string? @ListType, string? CenterID, string? EXPORT_LC_NO, string? BENName, string? USER_ID, string? Page, string? PageSize)
+        public async Task<EXLCConvertAcceptTermBillToWREFListResponse> GetAllList(string? @ListType, string? CenterID, string? EXPORT_LC_NO, string? BENName, string? USER_ID, string? Page, string? PageSize)
         {
-            DynamicParameters param = new();
+            EXLCConvertAcceptTermBillToWREFListResponse response = new EXLCConvertAcceptTermBillToWREFListResponse();
 
-            param.Add("@ListType", @ListType);
-            param.Add("@CenterID", CenterID);
-            param.Add("@EXPORT_LC_NO", EXPORT_LC_NO);
-            param.Add("@BENName", BENName);
-            param.Add("@USER_ID", USER_ID);
-            param.Add("@Page", Page);
-            param.Add("@PageSize", PageSize);
-
-            if (EXPORT_LC_NO == null)
+            // Validate
+            if (string.IsNullOrEmpty(@ListType) || string.IsNullOrEmpty(CenterID) || string.IsNullOrEmpty(Page) || string.IsNullOrEmpty(PageSize))
             {
-                param.Add("@EXPORT_LC_NO", "");
-            }
-            if (BENName == null)
-            {
-                param.Add("@BENName", "");
+                response.Code = Constants.RESPONSE_FIELD_REQUIRED;
+                response.Message = "ListType, CenterID, Page, PageSize is required";
+                response.Data = new List<Q_EXLCAcceptTermDueListPageRsp>();
+                return response;
             }
 
-            var results = await _db.LoadData<Q_EXLCAcceptTermDueListPageRsp, dynamic>(
-                        storedProcedure: "usp_q_EXLC_ConvertAcceptTermBilltoWREFListPage",
-                        param);
-            return results;
+            if (@ListType == "RELEASE" && string.IsNullOrEmpty(USER_ID))
+            {
+                response.Code = Constants.RESPONSE_FIELD_REQUIRED;
+                response.Message = "USER_ID is required for RELEASE";
+                response.Data = new List<Q_EXLCAcceptTermDueListPageRsp>();
+                return response;
+            }
+
+            // Call Store Procedure
+            try
+            {
+                DynamicParameters param = new();
+
+                param.Add("@ListType", @ListType);
+                param.Add("@CenterID", CenterID);
+                param.Add("@EXPORT_LC_NO", EXPORT_LC_NO);
+                param.Add("@BENName", BENName);
+                param.Add("@USER_ID", USER_ID);
+                param.Add("@Page", Page);
+                param.Add("@PageSize", PageSize);
+
+                if (EXPORT_LC_NO == null)
+                {
+                    param.Add("@EXPORT_LC_NO", "");
+                }
+                if (BENName == null)
+                {
+                    param.Add("@BENName", "");
+                }
+
+                var results = await _db.LoadData<Q_EXLCAcceptTermDueListPageRsp, dynamic>(
+                            storedProcedure: "usp_q_EXLC_ConvertAcceptTermBilltoWREFListPage",
+                            param);
+
+                response.Code = Constants.RESPONSE_OK;
+                response.Message = "Success";
+                response.Data = (List<Q_EXLCAcceptTermDueListPageRsp>)results;
+            }
+            catch (Exception e)
+            {
+                response.Code = Constants.RESPONSE_ERROR;
+                response.Message = e.ToString();
+                response.Data = new List<Q_EXLCAcceptTermDueListPageRsp>();
+            }
+            return response;
         }
 
         //[HttpGet("query")]
@@ -82,25 +116,39 @@ namespace ISPTF.API.Controllers.ExportLC
 
 
         [HttpGet("select")]
-        public async Task<ActionResult<PEXLCPPaymentRsp>> GetAllSelect(string? EXPORT_LC_NO,int? EVENT_NO, string? RECORD_TYPE, string? REC_STATUS, string? LFROM)
+        public async Task<EXLCConvertAcceptTermBillToWREFSelectResponse> GetAllSelect(string? EXPORT_LC_NO, int? EVENT_NO, string? RECORD_TYPE, string? REC_STATUS, string? LFROM)
         {
-            DynamicParameters param = new();
 
-            param.Add("@EXPORT_LC_NO", EXPORT_LC_NO);
-            param.Add("@EVENT_NO", EVENT_NO);
-            param.Add("@RECORD_TYPE", RECORD_TYPE);
-            param.Add("@REC_STATUS", REC_STATUS);
-            param.Add("@LFROM", LFROM);
+            EXLCConvertAcceptTermBillToWREFSelectResponse response = new EXLCConvertAcceptTermBillToWREFSelectResponse();
+            // Validate
+            if (string.IsNullOrEmpty(EXPORT_LC_NO) || EVENT_NO == null || string.IsNullOrEmpty(RECORD_TYPE) || string.IsNullOrEmpty(REC_STATUS) || string.IsNullOrEmpty(LFROM))
+            {
+                response.Code = Constants.RESPONSE_FIELD_REQUIRED;
+                response.Message = "EXPORT_LC_NO, EVENT_NO, RECORD_TYPE, REC_STATUS, LFROM is required";
+                response.Data = new PEXLCPPaymentRsp();
+                return response;
+            }
 
-            param.Add("@PExLcRsp", dbType: DbType.Int32,
-                       direction: System.Data.ParameterDirection.Output,
-                       size: 12800);
-
-            param.Add("@PEXLCPEXPaymentRsp", dbType: DbType.String,
-                       direction: System.Data.ParameterDirection.Output,
-                       size: 5215585);
+            // Call Store Procedure
             try
             {
+
+                DynamicParameters param = new();
+
+                param.Add("@EXPORT_LC_NO", EXPORT_LC_NO);
+                param.Add("@EVENT_NO", EVENT_NO);
+                param.Add("@RECORD_TYPE", RECORD_TYPE);
+                param.Add("@REC_STATUS", REC_STATUS);
+                param.Add("@LFROM", LFROM);
+
+                param.Add("@PExLcRsp", dbType: DbType.Int32,
+                           direction: System.Data.ParameterDirection.Output,
+                           size: 12800);
+
+                param.Add("@PEXLCPEXPaymentRsp", dbType: DbType.String,
+                           direction: System.Data.ParameterDirection.Output,
+                           size: 5215585);
+
                 var results = await _db.LoadData<PEXLCPPaymentRsp, dynamic>(
                            storedProcedure: "usp_pEXLC_ConvertAcceptTermBilltoWREF_Select",
                            param);
@@ -108,24 +156,32 @@ namespace ISPTF.API.Controllers.ExportLC
                 var PExLcRsp = param.Get<dynamic>("@PExLcRsp");
                 var pexlcpexpaymentrsp = param.Get<dynamic>("@PEXLCPEXPaymentRsp");
 
-                if (PExLcRsp > 0)
+                if (PExLcRsp > 0 && !string.IsNullOrEmpty(pexlcpexpaymentrsp))
                 {
-                    return Ok(pexlcpexpaymentrsp);
+                    PEXLCPPaymentRsp jsonResponse = JsonSerializer.Deserialize<PEXLCPPaymentRsp>(pexlcpexpaymentrsp);
+                    response.Code = Constants.RESPONSE_OK;
+                    response.Message = "Success";
+                    response.Data = jsonResponse;
+                    return response;
                 }
                 else
                 {
 
-                    ReturnResponse response = new();
-                    response.StatusCode = "400";
-                    response.Message = "EXPORT L/C NO does not exit";
-                    return BadRequest(response);
+                    response.Code = Constants.RESPONSE_ERROR;
+                    response.Message = "Error selecting ConvertAcceptTermBilltoWREF";
+                    response.Data = new PEXLCPPaymentRsp();
+                    return response;
                 }
 
+
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                return BadRequest(ex.Message);
+                response.Code = Constants.RESPONSE_ERROR;
+                response.Message = e.ToString();
+                response.Data = new PEXLCPPaymentRsp();
             }
+            return response;
         }
 
 
