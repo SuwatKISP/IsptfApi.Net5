7a9af1e472242dae7de06b3d07afed82c49a33c8
Natchapol <natchapol@findx.co.th>
2023-04-16 14:32:29 +0700

    Finishing LC 15

--- ISPTF.API/Controllers/ExportLC/PaymentOverDue/EXLCPaymentOverDueController.cs
+++ ISPTF.API/Controllers/ExportLC/PaymentOverDue/EXLCPaymentOverDueController.cs
@@ -12,7 +12,9 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
- 
+using ISPTF.Models.ExportLC.PaymentOverDue;
+using System.Text.Json;
+
 namespace ISPTF.API.Controllers.ExportLC
 {
     [ApiController]
@@ -26,53 +28,108 @@ namespace ISPTF.API.Controllers.ExportLC
         }
 
         [HttpGet("list")]
-        public async Task<IEnumerable<Q_EXLCPaymentOverDueListPageRsp>> GetAllList(string? @ListType, string? CenterID, string? EXPORT_LC_NO, string? BENName, string? USER_ID, string? Page, string? PageSize)
+        public async Task<EXLCPaymentOverDueListResponse> GetAllList(string? @ListType, string? CenterID, string? EXPORT_LC_NO, string? BENName, string? USER_ID, string? Page, string? PageSize)
         {
-            DynamicParameters param = new();
+            EXLCPaymentOverDueListResponse response = new EXLCPaymentOverDueListResponse();
 
-            param.Add("@ListType", @ListType);
-            param.Add("@CenterID", CenterID);
-            param.Add("@EXPORT_LC_NO", EXPORT_LC_NO);
-            param.Add("@BENName", BENName);
-            param.Add("@USER_ID", USER_ID);
-            param.Add("@Page", Page);
-            param.Add("@PageSize", PageSize);
-
-            if (EXPORT_LC_NO == null)
+            // Validate
+            if (string.IsNullOrEmpty(ListType) || string.IsNullOrEmpty(CenterID) || string.IsNullOrEmpty(Page) || string.IsNullOrEmpty(PageSize))
             {
-                param.Add("@EXPORT_LC_NO", "");
+                response.Code = Constants.RESPONSE_FIELD_REQUIRED;
+                response.Message = "ListType, CenterID, Page, PageSize is required";
+                response.Data = new List<Q_EXLCPaymentOverDueListPageRsp>();
+                return response;
             }
-            if (BENName == null)
+            if (ListType == "RELEASE" && string.IsNullOrEmpty(USER_ID))
             {
-                param.Add("@BENName", "");
+                response.Code = Constants.RESPONSE_FIELD_REQUIRED;
+                response.Message = "USER_ID is required";
+                response.Data = new List<Q_EXLCPaymentOverDueListPageRsp>();
+                return response;
             }
 
-            var results = await _db.LoadData<Q_EXLCPaymentOverDueListPageRsp, dynamic>(
-                        storedProcedure: "usp_q_EXLC_PaymentOverDueListPage",
-                        param);
-            return results;
+            try
+            {
+                DynamicParameters param = new();
+
+                param.Add("@ListType", @ListType);
+                param.Add("@CenterID", CenterID);
+                param.Add("@EXPORT_LC_NO", EXPORT_LC_NO);
+                param.Add("@BENName", BENName);
+                param.Add("@USER_ID", USER_ID);
+                param.Add("@Page", Page);
+                param.Add("@PageSize", PageSize);
+
+                if (EXPORT_LC_NO == null)
+                {
+                    param.Add("@EXPORT_LC_NO", "");
+                }
+                if (BENName == null)
+                {
+                    param.Add("@BENName", "");
+                }
+
+                var results = await _db.LoadData<Q_EXLCPaymentOverDueListPageRsp, dynamic>(
+                            storedProcedure: "usp_q_EXLC_PaymentOverDueListPage",
+                            param);
+
+                response.Code = Constants.RESPONSE_OK;
+                response.Message = "Success";
+                response.Data = (List<Q_EXLCPaymentOverDueListPageRsp>)results;
+
+                try
+                {
+                    response.Page = int.Parse(Page);
+                    response.Total = response.Data[0].RCount;
+                    response.TotalPage = Convert.ToInt32(Math.Ceiling(response.Total / decimal.Parse(PageSize)));
+                }
+                catch (Exception)
+                {
+                    response.Page = 0;
+                    response.Total = 0;
+                    response.TotalPage = 0;
+                }
+            }
+            catch (Exception e)
+            {
+                response.Code = Constants.RESPONSE_ERROR;
+                response.Message = e.ToString();
+                response.Data = new List<Q_EXLCPaymentOverDueListPageRsp>();
+            }
+            return response;
         }
 
         [HttpGet("select")]
-        public async Task<ActionResult<PEXLCPEXPaymentRsp>> GetAllSelect(string? EXPORT_LC_NO, int? EVENT_NO,  string? LFROM)
+        public async Task<EXLCPaymentOverDueSelectResponse> GetAllSelect(string? EXPORT_LC_NO, int? EVENT_NO, string? LFROM)
         {
-            DynamicParameters param = new();
+            EXLCPaymentOverDueSelectResponse response = new EXLCPaymentOverDueSelectResponse();
 
-            param.Add("@EXPORT_LC_NO", EXPORT_LC_NO);
-            param.Add("@EVENT_NO", EVENT_NO);
-            //param.Add("@RECORD_TYPE", RECORD_TYPE);
-            //param.Add("@REC_STATUS", REC_STATUS);
-            param.Add("@LFROM", LFROM);
-
-            param.Add("@PExLcRsp", dbType: DbType.Int32,
-                       direction: System.Data.ParameterDirection.Output,
-                       size: 12800);
-
-            param.Add("@PEXLCPEXPaymentRsp", dbType: DbType.String,
-                       direction: System.Data.ParameterDirection.Output,
-                       size: 5215585);
+            // Validate
+            if (string.IsNullOrEmpty(EXPORT_LC_NO) || EVENT_NO == null || string.IsNullOrEmpty(LFROM))
+            {
+                response.Code = Constants.RESPONSE_FIELD_REQUIRED;
+                response.Message = "EXPORT_LC_NO, EVENT_NO, LFROM is required";
+                response.Data = new PEXLCPEXPaymentRsp();
+                return response;
+            }
             try
             {
+                DynamicParameters param = new();
+
+                param.Add("@EXPORT_LC_NO", EXPORT_LC_NO);
+                param.Add("@EVENT_NO", EVENT_NO);
+                //param.Add("@RECORD_TYPE", RECORD_TYPE);
+                //param.Add("@REC_STATUS", REC_STATUS);
+                param.Add("@LFROM", LFROM);
+
+                param.Add("@PExLcRsp", dbType: DbType.Int32,
+                           direction: System.Data.ParameterDirection.Output,
+                           size: 12800);
+
+                param.Add("@PEXLCPEXPaymentRsp", dbType: DbType.String,
+                           direction: System.Data.ParameterDirection.Output,
+                           size: 5215585);
+
                 var results = await _db.LoadData<PEXLCPEXPaymentRsp, dynamic>(
                            storedProcedure: "usp_pEXLC_PaymentOverDue_Select",
                            param);
@@ -80,33 +137,30 @@ namespace ISPTF.API.Controllers.ExportLC
                 var PExLcRsp = param.Get<dynamic>("@PExLcRsp");
                 var pexlcpexpaymentrsp = param.Get<dynamic>("@PEXLCPEXPaymentRsp");
 
-                if (PExLcRsp > 0)
+                if (PExLcRsp > 0 && !string.IsNullOrEmpty(pexlcpexpaymentrsp))
                 {
-                    return Ok(pexlcpexpaymentrsp);
+                    PEXLCPEXPaymentRsp jsonResponse = JsonSerializer.Deserialize<PEXLCPEXPaymentRsp>(pexlcpexpaymentrsp);
+                    response.Code = Constants.RESPONSE_OK;
+                    response.Message = "Success";
+                    response.Data = jsonResponse;
+                    return response;
                 }
                 else
                 {
-
-                    ReturnResponse response = new();
-                    response.StatusCode = "400";
+                    response.Code = Constants.RESPONSE_ERROR;
                     response.Message = "EXPORT L/C NO does not exit";
-                    return BadRequest(response);
+                    response.Data = new PEXLCPEXPaymentRsp();
+                    return response;
                 }
-
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                return BadRequest(ex.Message);
+                response.Code = Constants.RESPONSE_ERROR;
+                response.Message = e.ToString();
+                response.Data = new PEXLCPEXPaymentRsp();
             }
+            return response;
         }
-
-
-
-
-
-
-
-
 
     }
 }
---
+++ ISPTF.Models/ExportLC/PaymentOverDue/EXLCPaymentOverDueListResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISPTF.Models.ExportLC.PaymentOverDue
+{
+    public class EXLCPaymentOverDueListResponse
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public List<Q_EXLCPaymentOverDueListPageRsp> Data { get; set; }
+        public int Total { get; set; }
+        public int Page { get; set; }
+        public int TotalPage { get; set; }
+    }
+}
