1dce61b508c489bd0b1c29213409470ef9c248d3
pattara.tongpan@gmail.com <pattara.tongpan@gmail.com>
2023-04-21 02:00:17 +0700

    exbc issue purchase -newlist -editlist -editselect -insert -update

---
+++ ISPTF.Models/ExportBC/PEXBCPPaymentResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ISPTF.Models.ExportBC
+{
+    public class PEXBCPPaymentResponse
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public PEXBCPPaymentRsp Data { get; set; }
+    }
+}
