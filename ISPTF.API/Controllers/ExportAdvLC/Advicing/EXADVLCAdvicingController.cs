cde4551d5f2e63767f82df9748cfcd7a356c9a71
Natchapol <natchapol@findx.co.th>
2023-05-08 22:07:08 +0700

    advlc select

--- ISPTF.API/Controllers/ExportAdvLC/Advicing/EXADVLCAdvicingController.cs
+++ ISPTF.API/Controllers/ExportAdvLC/Advicing/EXADVLCAdvicingController.cs
@@ -1,9 +1,7 @@
 ﻿using Dapper;
 using ISPTF.DataAccess.DbAccess;
 using ISPTF.Models;
-using ISPTF.Models.LoginRegis;
 using ISPTF.Models.ExportAdvLC;
-using ISPTF.Models.PurchasePayment;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,14 +9,15 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
+using ISPTF.Models.LoginRegis;
 using System.Transactions;
-using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
 
 namespace ISPTF.API.Controllers.ExportAdvLC
 {
-    //[Authorize]
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class EXADVLCAdvicingController : ControllerBase
@@ -30,43 +29,39 @@ namespace ISPTF.API.Controllers.ExportAdvLC
             _db = db;
             _context = context;
         }
-    }
-
-    [HttpGet("select")]
-    public async Task<ActionResult<PEXADResponse>> GetAllSelect(string? EXPORT_ADVICE_NO, string? RECORD_TYPE, string? REC_STATUS, string? EVENT_NO)
-    {
-        PEXADResponse response = new PEXADResponse();
-        var USER_ID = User.Identity.Name;
-        var claimsPrincipal = HttpContext.User;
-        var USER_CENTER_ID = claimsPrincipal.FindFirst("UserBranch").Value.ToString();
-
-        // Validate
-        if (string.IsNullOrEmpty(EXPORT_ADVICE_NO) || string.IsNullOrEmpty(RECORD_TYPE) || string.IsNullOrEmpty(REC_STATUS) || string.IsNullOrEmpty(EVENT_NO))
+        [HttpGet("select")]
+        public async Task<ActionResult<PEXADResponse>> Select(string? EXPORT_ADVICE_NO, string? RECORD_TYPE, string? REC_STATUS, string? EVENT_NO)
         {
-            response.Code = Constants.RESPONSE_FIELD_REQUIRED;
-            response.Message = "EXPORT_ADVICE_NO, RECORD_TYPE, REC_STATUS, EVENT_NO is required";
-            response.Data = new pExad();
-            return BadRequest(response);
-        }
+            PEXADResponse response = new PEXADResponse();
+            var USER_ID = User.Identity.Name;
+            var claimsPrincipal = HttpContext.User;
+            var USER_CENTER_ID = claimsPrincipal.FindFirst("UserBranch").Value.ToString();
 
-        DynamicParameters param = new();
-        param.Add("@EXPORT_ADVICE_NO", EXPORT_ADVICE_NO);
-        param.Add("@RECORD_TYPE", RECORD_TYPE);
-        param.Add("@REC_STATUS", REC_STATUS);
-        param.Add("@EVENT_NO", EVENT_NO);
-        //param.Add("@LFROM", LFROM);
+            // Validate
+            if (string.IsNullOrEmpty(EXPORT_ADVICE_NO) || string.IsNullOrEmpty(RECORD_TYPE) || string.IsNullOrEmpty(REC_STATUS) || string.IsNullOrEmpty(EVENT_NO))
+            {
+                response.Code = Constants.RESPONSE_FIELD_REQUIRED;
+                response.Message = "EXPORT_ADVICE_NO, RECORD_TYPE, REC_STATUS, EVENT_NO is required";
+                response.Data = new pExad();
+                return BadRequest(response);
+            }
 
-        using (var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
-        {
+            DynamicParameters param = new();
+            param.Add("@EXPORT_ADVICE_NO", EXPORT_ADVICE_NO);
+            param.Add("@RECORD_TYPE", RECORD_TYPE);
+            param.Add("@REC_STATUS", REC_STATUS);
+            param.Add("@EVENT_NO", EVENT_NO);
+            //param.Add("@LFROM", LFROM);
+
             try
             {
                 // Select pExad
-                var pExad = (from row in _context.pExads
-                             where row.EXPORT_ADVICE_NO == EXPORT_ADVICE_NO &&
-                                   row.RECORD_TYPE == RECORD_TYPE &&
-                                   row.REC_STATUS == REC_STATUS &&
-                                   row.EVENT_NO == EVENT_NO
-                             select row).FirstOrDefault();
+                var pExad = await (from row in _context.pExads
+                                   where row.EXPORT_ADVICE_NO == EXPORT_ADVICE_NO &&
+                                         row.RECORD_TYPE == RECORD_TYPE &&
+                                         row.REC_STATUS == REC_STATUS &&
+                                         row.EVENT_NO == int.Parse(EVENT_NO)
+                                   select row).FirstOrDefaultAsync();
                 if (pExad == null)
                 {
                     response.Code = Constants.RESPONSE_ERROR;
@@ -80,7 +75,7 @@ namespace ISPTF.API.Controllers.ExportAdvLC
                     return Ok(response);
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 response.Code = Constants.RESPONSE_ERROR;
                 response.Message = e.ToString();
@@ -89,4 +84,5 @@ namespace ISPTF.API.Controllers.ExportAdvLC
             return BadRequest(response);
         }
     }
+
 }
