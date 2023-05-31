8a8fcdbbfe590e2399d49e1b2b169620154945f1
Natchapol <natchapol@findx.co.th>
2023-05-31 17:15:07 +0700

    change default config in ISPTFContext

--- ISPTF.Models/LINQ/ISPTFContext.cs
+++ ISPTF.Models/LINQ/ISPTFContext.cs
@@ -375,8 +375,7 @@ namespace ISPTF.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=203.154.158.182;Database=ISPTF_PROD;User Id=sa;Password=ispadmin;");
+                optionsBuilder.UseSqlServer("Server=203.154.158.182;Database=ISPTF;User Id=sa;Password=ispadmin;");
             }
         }
 
