3b93cae4956b614bb661811ca605f7b36af8f1e0
Sarawut <sarawut@is-professional.com>
2023-06-06 18:07:27 +0700

    eve

---
+++ ISPTF.Models/LoginRegis/Q_IMTROGridDetailsRsp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISPTF.Models.LoginRegis
+{
+    public class Q_IMTROGridDetailsRsp
+    {
+        public string InvNumber { get; set; }
+        public string Invamount { get; set; }
+        public string InvUse { get; set; }
+    }
+}
---
+++ ISPTF.Models/LoginRegis/Q_IMTROInsGrdInvoiceRsp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISPTF.Models.LoginRegis
+{
+    public class Q_IMTROInsGrdInvoiceRsp
+    {
+        public string Customer { get; set; }
+        public string InvGroup { get; set; }
+        public string InvDate { get; set; }
+        public string InvNumber { get; set; }
+        public string InvCcy { get; set; }
+        public string Invamount { get; set; }
+        public string InvSupply { get; set; }
+        public string InvBalance { get; set; }
+    }
+}
