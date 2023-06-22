9f2beb088624a60b760551c51a3fad08ba89bd50
Sarawut <sarawut@is-professional.com>
2023-06-22 13:21:58 +0700

    eve commit

--- ISPTF.Models/LoginRegis/PDocRegInv.cs
+++ ISPTF.Models/LoginRegis/PDocRegInv.cs
@@ -9,8 +9,7 @@ namespace ISPTF.Models.LoginRegis
     public class PDocRegInv
     {
         public string? Reg_Docno { get; set; }
-        public int? Reg_Docseq { get; set; }
-        public string? reg_Docno { get; set; }
+        public int? Reg_DocSeq { get; set; }
         public string? CustCode { get; set; }
         public string? InvNumber { get; set; }
         public double? InvAmount { get; set; }
