cfd17e629318577cf03bfdba99c9aa6b1024bb6a
saravut vayurojn <v_saravut@hotmail.com>
2023-08-30 16:48:04 +0700

    Commit Susssi  BatchLog

---
+++ ISPTF.API/Controllers/BatchControl/BatchController.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using ISPTF.DataAccess.DbAccess;
+using ISPTF.Models;
+using ISPTF.Models.BatchControl;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ISPTF.API.Controllers.BatchControl
+{
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatchController : ControllerBase
+    {
+        private readonly ISqlDataAccess _db;
+        public BatchController(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("BatchLogQuery")]
+        public async Task<IEnumerable<BatchLogQuery>> GetAll(string WorkDate)
+        {
+            DynamicParameters param = new();
+            param.Add("@WorkDate", WorkDate);
+
+            var results = await _db.LoadData<BatchLogQuery, dynamic>(
+                        storedProcedure: "usp_Batch_LogQuery",
+                        param);
+            return results;
+        }
+        // BatchLogQuery
+        [HttpGet("BTBatchLogQuery")]
+        public async Task<IEnumerable<BTBatchLogQuery>> GetAllBT(string WorkDate)
+        {
+            DynamicParameters param = new();
+            param.Add("@WorkDate", WorkDate);
+
+            var results = await _db.LoadData<BTBatchLogQuery, dynamic>(
+                        storedProcedure: "usp_BatchBT_LogQuery",
+                        param);
+            return results;
+        }
+        // BatchLogQuery
+    }//main
+}
+
---
+++ ISPTF.Models/BatchControl/BatchLogQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISPTF.Models.BatchControl
+{
+    public class BatchLogQuery
+    {
+        public DateTime? RunDate { get; set; }
+        public string? StartTime { get; set; }
+        public string? LastTime { get; set; }
+        public int? JobNo { get; set; }
+        public string? JobName { get; set; }
+        public string? RunStat { get; set; }
+
+
+    }
+}
