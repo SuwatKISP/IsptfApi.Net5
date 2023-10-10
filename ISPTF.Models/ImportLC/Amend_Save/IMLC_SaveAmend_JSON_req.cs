186f60f8c42adaa244b23487f8630b617e3e849b
sumath <sumath.k@gmail.com>
2023-10-10 17:45:51 +0700

    SM

---
+++ ISPTF.Models/ImportLC/Amend_Save/IMLC_SaveAmend_JSON_req.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISPTF.Models.ImportLC
+{
+    public class IMLC_SaveAmend_JSON_req
+    {
+        public IMLC_SaveAmend_ListType_req ListType { get; set; }
+        public IMLC_SaveAmend_pIMLC_req pIMLC { get; set; }
+        public IMLC_SaveAmend_pIMLCGoods_req pIMLCGoods { get; set; }
+        public IMLC_SaveAmend_pIMLCCond_req pIMLCCond { get; set; }
+        public IMLC_SaveAmend_pIMLCDocs_req pIMLCDocs { get; set; }
+        public IMLC_SaveAmend_pSWIMLC_req pSWIMLC { get; set; }
+        public IMLC_SaveAmend_pPayment_req pPayment { get; set; }
+    }
+}
