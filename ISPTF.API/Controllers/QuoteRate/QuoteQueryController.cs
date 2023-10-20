b87e4bf63ce9cd9f1a477bf744a0d0e667024224
Sarawut <sarawut@is-professional.com>
2023-10-20 18:40:24 +0700

    commit eve

--- ISPTF.API/Controllers/QuoteRate/QuoteQueryController.cs
+++ ISPTF.API/Controllers/QuoteRate/QuoteQueryController.cs
@@ -65,7 +65,7 @@ namespace ISPTF.API.Controllers.QuoteRate
             return results;
         }
         // ListpageHist    }
-        [HttpGet("listpageLog")]
+
         public async Task<IEnumerable<QuoteQINTListLog>> GetAllG(string RateDate, int? Page, int? PageSize)
         {
             DynamicParameters param = new();
