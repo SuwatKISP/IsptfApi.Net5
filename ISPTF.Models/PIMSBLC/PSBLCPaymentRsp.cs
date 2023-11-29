8c99a3a9dff2c5270e068384eef4923546f01041
Natpaphon Sukitpaneenit <natpasukit@gmail.com>
2023-11-29 10:13:00 +0700

    Clean logger

---
+++ ISPTF.Models/PIMSBLC/PSBLCPaymentRsp.cs
@@ -0,0 +1,16 @@
+﻿using ISPTF.Models.PPayment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISPTF.Models.PIMSBLC
+{
+    public class PSBLCPaymentRsp
+    {
+        public PIMSBLC PIMSBLC { get; set; }
+        public PPaymentRsp PPayment { get; set; }
+
+    }
+}
